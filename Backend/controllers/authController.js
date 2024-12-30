9c27be0ef28891ad6d32ac344d6445c800cb8a04
Rahh010 <raahh024@gmail.com>
2024-12-30 22:23:53 +0530

    changes cors

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -5,34 +5,32 @@ const bcrypt = require("bcryptjs");
 exports.login = async (req, res) => {
   const { username, password } = req.body;
   try {
-    // Find the user by username
     const user = await User.findOne({ username });
     if (!user) return res.status(401).json({ message: "Invalid credentials" });
 
-    // Check if the password matches
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
 
-    // Generate a JWT token
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h", // Token expiration time
+      expiresIn: "1h",
     });
 
     // Set the token as an HTTP-only cookie
-    res
-      .cookie("authToken", token, {
-        httpOnly: true, // Prevent access via JavaScript
-        maxAge: 60 * 60 * 1000, // 1 hour in milliseconds
-        sameSite: 'None',
-        domain: '.vercel.app',  // Ensures the cookie is accessible across subdomains (like `manthings.vercel.app` and `manthingsserver.vercel.app`)      
-      })
-      .status(200)
-      .json({ message: "Login successful" });
+    res.cookie("authToken", token, {
+      httpOnly: true,
+      maxAge: 60 * 60 * 1000,  // 1 hour
+      sameSite: 'None',
+      secure: true,  // Ensure it's only sent over HTTPS
+      domain: '.vercel.app',  // For subdomains
+    })
+    .status(200)
+    .json({ message: "Login successful" });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server error" });
   }
 };
+
 
 
 exports.register = async (req, res) => {
