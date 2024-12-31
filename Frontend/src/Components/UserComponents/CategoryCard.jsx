467f179488032bec4a0420ed85722ce790c58ad1
Rahh010 <raahh024@gmail.com>
2024-12-31 19:28:03 +0530

    jpg changes to webp

--- Frontend/src/Components/UserComponents/CategoryCard.jsx
+++ Frontend/src/Components/UserComponents/CategoryCard.jsx
@@ -13,8 +13,8 @@ const CategoryCard = ({ imageSrc, highlightText, route }) => {
   return (
     <div className={ cc.categoryCard} onClick={handleClick}>
       <div className={cc.cardImage}>
-        <img className={ cc.img } src={imageSrc} alt="Category" />
-        <div className={cc.cardText}>{highlightText}</div>
+      <div className={cc.img} style={{ backgroundImage: `url(${imageSrc})` }} alt="Category"></div>
+      <div className={cc.cardText}>{highlightText}</div>
       </div>
     </div>
   );
